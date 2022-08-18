44310fb53e629e3c7368f898fccf3b83f55b335e
HuuTin <huutin1298@gmail.com>
2022-08-18 22:42:40 +0700

    Add router

--- index.js
+++ index.js
@@ -1407,6 +1407,305 @@ const dataVocabularyLesson = {
   ],
 };
 
+const practicePhoto = [
+  {
+    "id": 1,
+    "name": "Unit 1 : Photo Hiragana words",
+    "learned": "10",
+    "total": "10",
+    "listQuestion": [
+      {
+        "question": {
+          "id": 1,
+          "name": "a",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fa.mp3?alt=media&token=ca2bcace-7c04-4bfa-b2d2-c4d1975341a5",
+          "list_answer": {
+            "a": "い",
+            "b": "あ",
+            "c": "が",
+            "d": "た"
+          },
+          "answer": "b"
+        }
+      },
+      {
+        "question": {
+          "id": 2,
+          "name": "i",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fi.mp3?alt=media&token=5a0ec4e5-98ef-40e1-90bf-899382c12a63",
+          "list_answer": {
+            "a": "ぢ",
+            "b": "す",
+            "c": "ぎ",
+            "d": "い"
+          },
+          "answer": "d"
+        }
+      },
+      {
+        "question": {
+          "id": 3,
+          "name": "u",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fu.mp3?alt=media&token=679cc464-c14c-416f-a994-6cccd62ab1aa",
+          "list_answer": {
+            "a": "う",
+            "b": "ず",
+            "c": "づ",
+            "d": "に"
+          },
+          "answer": "a"
+        }
+      },
+      {
+        "question": {
+          "id": 4,
+          "name": "e",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fe.mp3?alt=media&token=24e43b6e-02d9-4d6b-a058-1a90ded7018f",
+          "list_answer": {
+            "a": "え",
+            "b": "な",
+            "c": "ば",
+            "d": "び"
+          },
+          "answer": "a"
+        }
+      },
+      {
+        "question": {
+          "id": 5,
+          "name": "o",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fo.mp3?alt=media&token=d995427a-f0e6-41f2-850d-98fdb937bc33",
+          "list_answer": {
+            "a": "び",
+            "b": "お",
+            "c": "ぶ ",
+            "d": "ぴ"
+          },
+          "answer": "b"
+        }
+      },
+      {
+        "question": {
+          "id": 6,
+          "name": "ka",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fka.mp3?alt=media&token=107a9134-2e3d-4d10-a6ba-ec91a6e484d4",
+          "list_answer": {
+            "a": "ふ",
+            "b": "む",
+            "c": "か",
+            "d": "べ"
+          },
+          "answer": "c"
+        }
+      },
+      {
+        "question": {
+          "id": 7,
+          "name": "ki",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fki.mp3?alt=media&token=37c92dbc-2535-44d9-9b4e-6ca7b758a5a3",
+          "list_answer": {
+            "a": "ふ",
+            "b": "き",
+            "c": "ぎ",
+            "d": "ひ"
+          },
+          "answer": "b"
+        }
+      },
+      {
+        "question": {
+          "id": 8,
+          "name": "ku",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fku.mp3?alt=media&token=305287ed-438f-4013-9acd-ad4d5a6e8b4c",
+          "list_answer": {
+            "a": "く",
+            "b": "け",
+            "c": "ぜ",
+            "d": "そ"
+          },
+          "answer": "a"
+        }
+      },
+      {
+        "question": {
+          "id": 9,
+          "name": "ke",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fke.mp3?alt=media&token=0a96bfef-5cee-4714-acec-0dfff89f15bf",
+          "list_answer": {
+            "a": "と",
+            "b": "で",
+            "c": "け",
+            "d": "ね"
+          },
+          "answer": "c"
+        }
+      },
+      {
+        "question": {
+          "id": 10,
+          "name": "ko",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fko.mp3?alt=media&token=659db03f-2870-42ad-9d19-35ef8c603667",
+          "list_answer": {
+            "a": "ぜ",
+            "b": "こ",
+            "c": "の",
+            "d": "び"
+          },
+          "answer": "b"
+        }
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Unit 2 : Photo Hiragana words",
+    "learned": "10",
+    "total": "10",
+    "listQuestion": [
+      {
+        "question": {
+          "id": 1,
+          "name": "sa",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fsa.mp3?alt=media&token=d2365b73-70b7-473c-a5e6-7f25278c409c",
+          "list_answer": {
+            "a": "さ",
+            "b": "あ",
+            "c": "が",
+            "d": "た"
+          },
+          "answer": "a"
+        }
+      },
+      {
+        "question": {
+          "id": 2,
+          "name": "shi",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fshi.mp3?alt=media&token=954a05ca-fe3c-48c4-b6eb-fd2eb96dc964",
+          "list_answer": {
+            "a": "ぢ",
+            "b": "し",
+            "c": "ぎ",
+            "d": "い"
+          },
+          "answer": "b"
+        }
+      },
+      {
+        "question": {
+          "id": 3,
+          "name": "su",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fsu.mp3?alt=media&token=da22ec1b-4c05-4610-88ea-34de533e00a1",
+          "list_answer": {
+            "a": "う",
+            "b": "す",
+            "c": "づ",
+            "d": "に"
+          },
+          "answer": "b"
+        }
+      },
+      {
+        "question": {
+          "id": 4,
+          "name": "se",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fse.mp3?alt=media&token=824d737f-ae8f-4adc-ba57-4ab50e263499",
+          "list_answer": {
+            "a": "え",
+            "b": "な",
+            "c": "せ",
+            "d": "び"
+          },
+          "answer": "c"
+        }
+      },
+      {
+        "question": {
+          "id": 5,
+          "name": "o",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fo.mp3?alt=media&token=d995427a-f0e6-41f2-850d-98fdb937bc33",
+          "list_answer": {
+            "a": "び",
+            "b": "お",
+            "c": "ぶ ",
+            "d": "ぴ"
+          },
+          "answer": "b"
+        }
+      },
+      {
+        "question": {
+          "id": 6,
+          "name": "so",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fso.mp3?alt=media&token=4b2a6149-bb74-43cb-b330-2aa4fc2b6d7a",
+          "list_answer": {
+            "a": "ふ",
+            "b": "む",
+            "c": "か",
+            "d": "そ"
+          },
+          "answer": "d"
+        }
+      },
+      {
+        "question": {
+          "id": 7,
+          "name": "ta",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fta.mp3?alt=media&token=7543f176-4e42-41d0-b828-c9e82751abc4",
+          "list_answer": {
+            "a": "ふ",
+            "b": "き",
+            "c": "た",
+            "d": "ひ"
+          },
+          "answer": "c"
+        }
+      },
+      {
+        "question": {
+          "id": 8,
+          "name": "chi",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fchi.mp3?alt=media&token=82b808fb-cb87-4fa4-9d42-ac54238c023e",
+          "list_answer": {
+            "a": "ち",
+            "b": "け",
+            "c": "ぜ",
+            "d": "そ"
+          },
+          "answer": "a"
+        }
+      },
+      {
+        "question": {
+          "id": 9,
+          "name": "tsu",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Ftsu.mp3?alt=media&token=71f76425-4a0c-40f7-b023-4df2035ded32",
+          "list_answer": {
+            "a": "と",
+            "b": "つ",
+            "c": "け",
+            "d": "ね"
+          },
+          "answer": "b"
+        }
+      },
+      {
+        "question": {
+          "id": 10,
+          "name": "te",
+          "sound": "https://firebasestorage.googleapis.com/v0/b/learningjapanalphabet.appspot.com/o/sound%2Fte.mp3?alt=media&token=762bcd06-edaf-4adb-baa2-f88f0b7c4d43",
+          "list_answer": {
+            "a": "ぜ",
+            "b": "て",
+            "c": "の",
+            "d": "び"
+          },
+          "answer": "b"
+        }
+      }
+    ]
+  }
+];
+
 app.get("/hiragana", function (req, res) {
   res.send(dataHiragana);
 });
@@ -1418,5 +1717,9 @@ app.get("/katakana", function (req, res) {
 app.get("/vocabulary-lesson", function (req, res) {
   res.send(dataVocabularyLesson);
 });
+
+app.get("/practicephoto", function (req, res) {
+  res.send(practicePhoto);
+})
 
 app.listen(PORT);
